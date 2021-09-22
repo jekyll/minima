bb0bb1b6bc4f137d1a37be181e69ae59c0ed920a
Gyan Lakhwani <gyanlakhwani@gmail.com>
2021-09-22 21:29:31 +0530

    Try to fix pell

--- assets/pell.js
+++ assets/pell.js
@@ -1,47 +1,88 @@
-const defaultParagraphSeparatorString = 'defaultParagraphSeparator'
-const formatBlock = 'formatBlock'
-const addEventListener = (parent, type, listener) => parent.addEventListener(type, listener)
-const appendChild = (parent, child) => parent.appendChild(child)
-const createElement = tag => document.createElement(tag)
-const queryCommandState = command => document.queryCommandState(command)
-const queryCommandValue = command => document.queryCommandValue(command)
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.pell = {})));
+}(this, (function (exports) { 'use strict';
 
-export const exec = (command, value = null) => document.execCommand(command, false, value)
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-const defaultActions = {
+var defaultParagraphSeparatorString = 'defaultParagraphSeparator';
+var formatBlock = 'formatBlock';
+var addEventListener = function addEventListener(parent, type, listener) {
+  return parent.addEventListener(type, listener);
+};
+var appendChild = function appendChild(parent, child) {
+  return parent.appendChild(child);
+};
+var createElement = function createElement(tag) {
+  return document.createElement(tag);
+};
+var queryCommandState = function queryCommandState(command) {
+  return document.queryCommandState(command);
+};
+var queryCommandValue = function queryCommandValue(command) {
+  return document.queryCommandValue(command);
+};
+
+var exec = function exec(command) {
+  var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+  return document.execCommand(command, false, value);
+};
+
+var defaultActions = {
   bold: {
     icon: '<b>B</b>',
     title: 'Bold',
-    state: () => queryCommandState('bold'),
-    result: () => exec('bold')
+    state: function state() {
+      return queryCommandState('bold');
+    },
+    result: function result() {
+      return exec('bold');
+    }
   },
   italic: {
     icon: '<i>I</i>',
     title: 'Italic',
-    state: () => queryCommandState('italic'),
-    result: () => exec('italic')
+    state: function state() {
+      return queryCommandState('italic');
+    },
+    result: function result() {
+      return exec('italic');
+    }
   },
   underline: {
     icon: '<u>U</u>',
     title: 'Underline',
-    state: () => queryCommandState('underline'),
-    result: () => exec('underline')
+    state: function state() {
+      return queryCommandState('underline');
+    },
+    result: function result() {
+      return exec('underline');
+    }
   },
   strikethrough: {
     icon: '<strike>S</strike>',
     title: 'Strike-through',
-    state: () => queryCommandState('strikeThrough'),
-    result: () => exec('strikeThrough')
+    state: function state() {
+      return queryCommandState('strikeThrough');
+    },
+    result: function result() {
+      return exec('strikeThrough');
+    }
   },
   heading1: {
     icon: '<b>H1</b>',
     title: 'Heading 1',
-    result: () => exec(formatBlock, '<h1>')
+    result: function result() {
+      return exec(formatBlock, '<h1>');
+    }
   },
   heading2: {
     icon: '<b>H2</b>',
     title: 'Heading 2',
-    result: () => exec(formatBlock, '<h2>')
+    result: function result() {
+      return exec(formatBlock, '<h2>');
+    }
   },
   heading3: {
     icon: '<b>H3</b>',
@@ -61,114 +102,138 @@ const defaultActions = {
   paragraph: {
     icon: '&#182;',
     title: 'Paragraph',
-    result: () => exec(formatBlock, '<p>')
+    result: function result() {
+      return exec(formatBlock, '<p>');
+    }
   },
   quote: {
     icon: '&#8220; &#8221;',
     title: 'Quote',
-    result: () => exec(formatBlock, '<blockquote>')
+    result: function result() {
+      return exec(formatBlock, '<blockquote>');
+    }
   },
   olist: {
     icon: '&#35;',
     title: 'Ordered List',
-    result: () => exec('insertOrderedList')
+    result: function result() {
+      return exec('insertOrderedList');
+    }
   },
   ulist: {
     icon: '&#8226;',
     title: 'Unordered List',
-    result: () => exec('insertUnorderedList')
+    result: function result() {
+      return exec('insertUnorderedList');
+    }
   },
   code: {
     icon: '&lt;/&gt;',
     title: 'Code',
-    result: () => exec(formatBlock, '<pre>')
+    result: function result() {
+      return exec(formatBlock, '<pre>');
+    }
   },
   line: {
     icon: '&#8213;',
     title: 'Horizontal Line',
-    result: () => exec('insertHorizontalRule')
+    result: function result() {
+      return exec('insertHorizontalRule');
+    }
   },
   link: {
     icon: '&#128279;',
     title: 'Link',
-    result: () => {
-      const url = window.prompt('Enter the link URL')
-      if (url) exec('createLink', url)
+    result: function result() {
+      var url = window.prompt('Enter the link URL');
+      if (url) exec('createLink', url);
     }
   },
   image: {
     icon: '&#128247;',
     title: 'Image',
-    result: () => {
-      const url = window.prompt('Enter the image URL')
-      if (url) exec('insertImage', url)
+    result: function result() {
+      var url = window.prompt('Enter the image URL');
+      if (url) exec('insertImage', url);
     }
   }
-}
+};
 
-const defaultClasses = {
+var defaultClasses = {
   actionbar: 'pell-actionbar',
   button: 'pell-button',
   content: 'pell-content',
   selected: 'pell-button-selected'
-}
+};
 
-export const init = settings => {
-  const actions = settings.actions
-    ? (
-      settings.actions.map(action => {
-        if (typeof action === 'string') return defaultActions[action]
-        else if (defaultActions[action.name]) return { ...defaultActions[action.name], ...action }
-        return action
-      })
-    )
-    : Object.keys(defaultActions).map(action => defaultActions[action])
+var init = function init(settings) {
+  var actions = settings.actions ? settings.actions.map(function (action) {
+    if (typeof action === 'string') return defaultActions[action];else if (defaultActions[action.name]) return _extends({}, defaultActions[action.name], action);
+    return action;
+  }) : Object.keys(defaultActions).map(function (action) {
+    return defaultActions[action];
+  });
 
-  const classes = { ...defaultClasses, ...settings.classes }
+  var classes = _extends({}, defaultClasses, settings.classes);
 
-  const defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || 'div'
+  var defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || 'div';
 
-  const actionbar = createElement('div')
-  actionbar.className = classes.actionbar
-  appendChild(settings.element, actionbar)
+  var actionbar = createElement('div');
+  actionbar.className = classes.actionbar;
+  appendChild(settings.element, actionbar);
 
-  const content = settings.element.content = createElement('div')
-  content.contentEditable = true
-  content.className = classes.content
-  content.oninput = ({ target: { firstChild } }) => {
-    if (firstChild && firstChild.nodeType === 3) exec(formatBlock, `<${defaultParagraphSeparator}>`)
-    else if (content.innerHTML === '<br>') content.innerHTML = ''
-    settings.onChange(content.innerHTML)
-  }
-  content.onkeydown = event => {
+  var content = settings.element.content = createElement('div');
+  content.contentEditable = true;
+  content.className = classes.content;
+  content.oninput = function (_ref) {
+    var firstChild = _ref.target.firstChild;
+
+    if (firstChild && firstChild.nodeType === 3) exec(formatBlock, '<' + defaultParagraphSeparator + '>');else if (content.innerHTML === '<br>') content.innerHTML = '';
+    settings.onChange(content.innerHTML);
+  };
+  content.onkeydown = function (event) {
     if (event.key === 'Enter' && queryCommandValue(formatBlock) === 'blockquote') {
-      setTimeout(() => exec(formatBlock, `<${defaultParagraphSeparator}>`), 0)
+      setTimeout(function () {
+        return exec(formatBlock, '<' + defaultParagraphSeparator + '>');
+      }, 0);
     }
-  }
-  appendChild(settings.element, content)
+  };
+  appendChild(settings.element, content);
 
-  actions.forEach(action => {
-    const button = createElement('button')
-    button.className = classes.button
-    button.innerHTML = action.icon
-    button.title = action.title
-    button.setAttribute('type', 'button')
-    button.onclick = () => action.result() && content.focus()
+  actions.forEach(function (action) {
+    var button = createElement('button');
+    button.className = classes.button;
+    button.innerHTML = action.icon;
+    button.title = action.title;
+    button.setAttribute('type', 'button');
+    button.onclick = function () {
+      return action.result() && content.focus();
+    };
 
     if (action.state) {
-      const handler = () => button.classList[action.state() ? 'add' : 'remove'](classes.selected)
-      addEventListener(content, 'keyup', handler)
-      addEventListener(content, 'mouseup', handler)
-      addEventListener(button, 'click', handler)
+      var handler = function handler() {
+        return button.classList[action.state() ? 'add' : 'remove'](classes.selected);
+      };
+      addEventListener(content, 'keyup', handler);
+      addEventListener(content, 'mouseup', handler);
+      addEventListener(button, 'click', handler);
     }
 
-    appendChild(actionbar, button)
-  })
+    appendChild(actionbar, button);
+  });
 
-  if (settings.styleWithCSS) exec('styleWithCSS')
-  exec(defaultParagraphSeparatorString, defaultParagraphSeparator)
+  if (settings.styleWithCSS) exec('styleWithCSS');
+  exec(defaultParagraphSeparatorString, defaultParagraphSeparator);
 
-  return settings.element
-}
+  return settings.element;
+};
 
-export default { exec, init }
+var pell = { exec: exec, init: init };
+
+exports.exec = exec;
+exports.init = init;
+exports['default'] = pell;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
